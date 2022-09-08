c6e4f734e51fff8017493231f685596999cbadeb
Vinicius Prado da Fonseca <vpradodafons@mun.ca>
2022-09-08 16:16:02 -0230

    Renaming and updating some files

--- input_scanf.c
+++ input_scanf.c
@@ -9,8 +9,10 @@ int main() {
     int my_var; // variable to store the input
 
     printf("Insert a string\n");
+    
+    // scanf need the variable address "&"
 //    scanf("%s", my_var); // scanf reading string
-    scanf("%d", &my_var); // scanf reading string
+    scanf("%d", &my_var); // scanf reading integer
     
 //    printf("you entered: %s\n", my_var);
     printf("you entered: %d\n", my_var);
